90cfa023963c6323fcbbb1c574075bad9862109c
Александр Кошелев <alexdevelwork@gmail.com>
2025-08-23 16:58:10 +0500

    fix: изменил api url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-const backendUrl = 'http://localhost:3002'
+const backendUrl = import.meta.env.VITE_API_URL
 
 // https://vite.dev/config/
 export default defineConfig({
