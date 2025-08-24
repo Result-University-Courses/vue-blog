b7902aaede98815f122ff03437d754444b627508
Александр Кошелев <alexdevelwork@gmail.com>
2025-08-24 19:03:25 +0500

    fix: исправил порт

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-const backendUrl = "http://localhost:3002"
+const backendUrl = "http://localhost:80"
 
 // https://vite.dev/config/
 export default defineConfig({
